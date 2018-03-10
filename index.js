d8135923d7c12e65c4f2c32bbefda6e569cbd035
J.W. Clark <jwclark@rockhursths.edu>
2018-03-09 23:47:52 -0600

    Add button click event log

--- index.js
+++ index.js
@@ -970,12 +970,15 @@ var loadJSON = function(tab) {
 
 window.onload = function() {
     document.getElementById('btnOne').onclick = function() {
+        console.log('one');
         loadJSON(1);
     }
     document.getElementById('btnTwo').onclick = function() {
+        console.log('two');
         loadJSON(2);
     }
     document.getElementById('btnThree').onclick = function() {
+        console.log('three');
         loadJSON(3);
     }
 }
