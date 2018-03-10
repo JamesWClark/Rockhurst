b44f38f75c082152d29b08101c60efeabad240c3
J.W. Clark <jwclark@rockhursths.edu>
2018-03-10 09:19:57 -0600

    Refactor app files

---
+++ speakers.js
@@ -0,0 +1,11 @@
+// pointless?
+var speakers = {
+    "AL GREEN": {
+        name: "Al Green",
+        title: "U.S. representative, Texas"
+    },
+    "ALEX SCHRIVER": {
+        name: "Alex Schriver",
+        title: "College Republican National Committee chairman"
+    }
+};
---
+++ topics.js
@@ -0,0 +1,138 @@
+// hard-coded topics
+var topics = [
+    {
+        name: "PD",
+        re: /\b(PD)\b/gi,
+        x: 558,
+        y: 181
+    },
+
+    {
+        name: "Greg",
+        re: /\b(Greg)\b/gi,
+        x: 123,
+        y: 181
+    },
+    {
+        name: "Education",
+        re: /\b(Education)\b/gi,
+        x: 43,
+        y: 203
+    },
+    {
+        name: "Math",
+        re: /\bMath\b/gi,
+        x: 58,
+        y: 265,
+        arrow: "Math"
+    },
+    {
+        name: "Helpful",
+        re: /\b(Helpful)\b/gi,
+        x: 618,
+        y: 201
+    },
+    {
+        name: "Help",
+        re: /\b(Help)\b/gi,
+        x: 361,
+        y: 256
+    },
+    {
+        name: "Rockhurst",
+        re: /\bRockhurst\b/gi,
+        x: 81,
+        y: 235
+    },
+    {
+        name: "SPED",
+        re: /\bSPED\b/gi,
+        x: 692,
+        y: 319,
+        arrow: "SPED"
+    },
+    {
+        name: "teacher",
+        re: /\b(teacher)\b/gi,
+        x: 364,
+        y: 179
+    },
+    {
+        name: "Church",
+        re: /\b(church)\b/gi,
+        x: 862,
+        y: 217
+    },
+    {
+        name: "instructors",
+        re: /\b(instructors)\b/gi,
+        x: 620,
+        y: 339
+    },
+    {
+        name: "Fellow",
+        re: /\b(Fellow)\b/gi,
+        x: 410,
+        y: 340
+    },
+    {
+        name: "Education",
+        re: /\b(Education)\b/gi,
+        x: 267,
+        y: 218
+    },
+    {
+        name: "Meet",
+        re: /\b(Meet)\b/gi,
+        x: 393,
+        y: 224
+    },
+    {
+        name: "engage",
+        re: /\bengage\b/gi,
+        x: 73,
+        y: 206
+    },
+    {
+        name: "Witness",
+        re: /\bwitness\b/gi,
+        x: 721,
+        y: 230
+    },
+    {
+        name: "perspectives",
+        re: /\b(perspectives)\b/gi,
+        x: 109,
+        y: 212
+    },
+    {
+        name: "Faith",
+        re: /\b(faith)\b/gi,
+        x: 650,
+        y: 246
+    },
+    {
+        name: "activities",
+        re: /\b(famil[a-z]+)\b/gi,
+        x: 401,
+        y: 113
+    },
+    {
+        name: "Great",
+        re: /\b(Great)\b/gi,
+        x: 214,
+        y: 248
+    },
+    {
+        name: "hometown",
+        re: /\b(hometown)\b/gi,
+        x: 829,
+        y: 215
+    },
+    {
+        name: "recommend",
+        re: /\b(recommend)\b/gi,
+        x: 139,
+        y: 177
+    }
+];
