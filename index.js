b5fa595c5f49a5fb4e8a2632fcdb27f69e2f9547
J.W. Clark <jwclark@rockhursths.edu>
2018-03-11 01:36:26 -0600

    Single function app

---
+++ index.js
@@ -0,0 +1,5 @@
+var source1 = 'https://docs.google.com/spreadsheets/d/1Ur0IGvFhrhPOIUPJx0LpauCSdEkJeMkVvIB4gj_Bx2I/edit#gid=945409335';
+var source2 = 'https://docs.google.com/spreadsheets/d/1aJ0E-qBDO3BABiQlqh4vXSIrqjHKCWEQ2egmsHzOsDM/edit#gid=1217055117';
+
+var viz = new TwoPartyVisual();
+viz.loadPearDeckSheets(source1, source2)
