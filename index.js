a9695a83bf423ed141bde08ccdeb9607d863b0d5
J.W. Clark <jwclark@rockhursths.edu>
2018-03-12 22:17:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// these sources need to be published on the web for this app to work correctly.
 var source1 = 'https://docs.google.com/spreadsheets/d/1Ur0IGvFhrhPOIUPJx0LpauCSdEkJeMkVvIB4gj_Bx2I/edit#gid=945409335';
 var source2 = 'https://docs.google.com/spreadsheets/d/1aJ0E-qBDO3BABiQlqh4vXSIrqjHKCWEQ2egmsHzOsDM/edit#gid=1217055117';
 
