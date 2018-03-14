a3be6c0ef670befe76f7aa96a1a0d20559f6031d
Greg Owsley <gowsley@rockhursths.edu>
2018-03-13 19:50:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // these sources need to be published on the web for this app to work correctly.
-var source1 = 'https://docs.google.com/spreadsheets/d/1Ur0IGvFhrhPOIUPJx0LpauCSdEkJeMkVvIB4gj_Bx2I/edit#gid=945409335';
+var source1 = 'https://docs.google.com/spreadsheets/d/1vNL_halMeQFD7PTLnpZ0iv3dTKGLTRw5oAXCvFRyH3c/edit?usp=sharing';
 var source2 = 'https://docs.google.com/spreadsheets/d/1aJ0E-qBDO3BABiQlqh4vXSIrqjHKCWEQ2egmsHzOsDM/edit#gid=1217055117';
 
 var viz = new TwoPartyVisual();
