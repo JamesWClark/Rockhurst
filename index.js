fa44b028da055782017a10b8223220b48aba252a
Greg Owsley <gowsley@rockhursths.edu>
2018-03-14 14:13:50 -0500

    Try again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // these sources need to be published on the web for this app to work correctly.
 var source1 = 'https://docs.google.com/spreadsheets/d/1vNL_halMeQFD7PTLnpZ0iv3dTKGLTRw5oAXCvFRyH3c/edit?usp=sharing';
-var source2 = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTNeg_brP3cQXeY--l7x_ypf4EvmLKgskDceKhwC-qbvdp2eT2nafj9-_8z8Xb1cjIcoC63YWKrgSyo/pubhtml';
+var source2 = 'https://docs.google.com/spreadsheets/d/1pdwvdwZUdiro9JP318D1XjlsehoqtSHR0qzMc5hm0Ug/edit#gid=614798975';
 
 var viz = new TwoPartyVisual();
 viz.loadPearDeckSheets(source1, source2)
